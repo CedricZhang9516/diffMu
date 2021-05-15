5e008e84b6a8b91f70ea0c93e3d212409317c0cb
CedricZhang <zhangce9516@gmail.com>
2021-05-15 20:45:42 +0900

    Update RunAction.cc

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -24,29 +24,32 @@ RunAction::~RunAction()
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void RunAction::BeginOfRunAction(const G4Run* aRun)
-{ 
+{
   tickStart = clock();
   time(&rawtime);
   G4cout << G4endl;
   G4cout << "### Run " << aRun->GetRunID() << " started at " << ctime(&rawtime);
-    
+
   //histograms
   //
-  fHistoManager->book(); 
+  fHistoManager->book();
+  G4cout << "fHistoManager->book() done" <<G4endl;
   fHistoManager->ZeroEventsPrimary();
+  G4cout << "fHistoManager->ZeroEventsPrimary() done" <<G4endl;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void RunAction::EndOfRunAction(const G4Run* aRun)
 {
+  G4cout << "start EndOfRunAction" <<G4endl;
   G4int NbOfEvents = aRun->GetNumberOfEvent();
   if (NbOfEvents == 0) return;
-      
+
   //save histograms
   //
   fHistoManager->PrintStatistic();
-  fHistoManager->save(); 
+  fHistoManager->save();
   tickEnd = clock() - tickStart;
   G4double telapsed = (float(tickEnd)/CLOCKS_PER_SEC);
   time(&rawtime);
@@ -54,7 +57,7 @@ void RunAction::EndOfRunAction(const G4Run* aRun)
   G4cout << "### Run " << aRun->GetRunID() << " ended at " << ctime(&rawtime);
   G4cout << "### Elapsed time " << telapsed << " seconds." << G4endl;
   G4cout << G4endl;
- 
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
